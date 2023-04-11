a1f2ab7f7ced8272a1f0a22e7c01e828101918e6
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-04-11 20:57:30 -0300

    fix: fix appointment rootdir

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  roots: ['<rootDir/src>'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnviroment: 'node',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
